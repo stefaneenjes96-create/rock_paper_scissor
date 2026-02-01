89c5f62be192b0ae6d3839d32c9a747606891165
Stefan Eenjes <stefaneenjes96@gmail.com>
2026-01-31 21:10:13 -0900

    Add pseudocode to script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+
+/* 
+Create a rock-paper-scissor game in the console.
+
+1. Welcome player and ask if the user wants to play.
+2. use a prompt thats ask for a yes or a no.
+3. if user says no taunt the user into playing anyway.
+4. if user says yes ask if he knows the rules.
+5. explain rules if necesary.
+6. start game and ask user for move.
+7. create a random move for the computer.
+8. compare the moves and decide the result.
+9. show result to user.
+10. ask if they want to play again.
+11. if yes repeat steps 6 trhough 10. 
+12. else say goodbye.
+*/
